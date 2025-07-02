2a94a471cd39dc8e6ba0daa4480113770d7e388b
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-07-02 01:53:52 +0100

    Add files via upload

---
+++ batch2.js
@@ -0,0 +1,12 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAeNrRGQpLaQ0fGDFOrpfQaD3t_k0akgIM",
+    authDomain: "sib-batch2.firebaseapp.com",
+    projectId: "sib-batch2",
+    storageBucket: "sib-batch2.firebasestorage.app",
+    messagingSenderId: "472226493442",
+    appId: "1:472226493442:web:78375bbe9fc1cabaa0e31b",
+    measurementId: "G-3T2QB03T3L"
+  };
+
+  
